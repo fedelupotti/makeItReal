f6862934cad77148a03761d5f4ed0670635ec530
Federico Lupotti <flupotti@applica.dev>
2024-01-09 13:30:11 -0300

    Initial Commit

---
+++ MakeItReal/MakeItRealApp.swift
@@ -0,0 +1,17 @@
+//
+//  MakeItRealApp.swift
+//  MakeItReal
+//
+//  Created by Federico Lupotti on 09/01/24.
+//
+
+import SwiftUI
+
+@main
+struct MakeItRealApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
